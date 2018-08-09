6e45f50ec4f5706d119de9b31304242f5457b133
Emre Cengel <ecengel@comwebinc.net>
2018-08-08 23:54:02 -0400

    Simple test method added for demonstration purposes.

---
+++ Tests.Fluentley.QueryBuilder/QueryBuilderTest.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Fluentley.QueryBuilder;
+using Fluentley.QueryBuilder.Models;
+using Fluentley.QueryBuilder.Options;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tests.Fluentley.QueryBuilder
+{
+    [TestClass]
+    public class QueryBuilderTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var list = new List<Person>
+            {
+                new Person("Alice", 20),
+                new Person("Bob", 30),
+                new Person("Patricia", 40),
+                new Person("Bill", 50),
+                new Person("Gabriealla", 50)
+            };
+
+            Action<IQueryOption<Person>> queryOption = option => option
+                    .QueryBy(query => query.Where(x => x.Name.ToLower().Contains("a")))
+                    .Paging(0, 1)
+                    .DynamicWhere("x=> x.Name != \"Gabriealla\"")
+                ;
+
+            IQueryResult<Person> queryResult = list.QueryOn(queryOption);
+            Assert.AreEqual(1, queryResult.Data.Count());
+        }
+
+        public class Person
+        {
+            public Person(string name, int age)
+            {
+                Name = name;
+                Age = age;
+            }
+
+            public string Name { get; set; }
+            public int Age { get; set; }
+        }
+    }
+}
